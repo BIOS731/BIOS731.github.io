1cebfc1dba179067622f25fccae05de000422a5a
julia-wrobel <julia.wrobel@cuanschutz.edu>
2026-02-19 11:45:50 -0500

    updated code file

--- labs/EM_GMM_test.R
+++ labs/EM_GMM_test.R
@@ -28,13 +28,11 @@ fit_mclust$parameters$variance
 ### mixtools does interface with bootstrap example
 library(mixtools)
 
-
 fit <- normalmixEM(y, k = 2)
 
 # Parametric bootstrap
 boot <- boot.se(fit, B = 500)
 
-boot$se
-boot$var
-boot$ci
 boot$mu.se
+
+
