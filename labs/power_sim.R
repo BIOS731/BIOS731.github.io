201314d5cff1d549dad9f47229c9ee5a2866c996
julia-wrobel <julia.wrobel@cuanschutz.edu>
2026-04-01 09:14:28 -0400

    updated slides

--- labs/power_sim.R
+++ labs/power_sim.R
@@ -13,56 +13,57 @@
 
 suppressPackageStartupMessages(library(tidyverse))
 
-wd <- getwd()
+wd = getwd()
 
 if (substring(wd, 2, 6) == "Users") {
-  doLocal <- TRUE
+  doLocal = TRUE
 } else {
-  doLocal <- FALSE
+  doLocal = FALSE
 }
 
 ###############################################################
 ## define simulation parameters
 ###############################################################
 
-nsim  <- 1000        # number of simulated datasets per scenario
-delta <- 0.5         # true standardized effect size (mean difference / SD)
-sigma <- 1           # within-group standard deviation
-alpha <- 0.05        # significance level for each test
+nsim  = 1000        # number of simulated datasets per scenario
+delta = 0.5         # true standardized effect size (mean difference / SD)
+sigma = 1           # within-group standard deviation
+alpha = 0.05        # significance level for each test
 
 # grid of sample sizes (per group) to evaluate
-n_vec <- c(25, 50, 100, 200)
+n_vec = c(25, 50, 100, 200)
 
 ###############################################################
 ## select scenario
 ###############################################################
 
 if (doLocal) {
-  scenario <- 1
+  scenario = 1
 } else {
-  scenario <- as.numeric(commandArgs(trailingOnly = TRUE))
+  ### passes array index
+  scenario = as.numeric(commandArgs(trailingOnly = TRUE))
 }
 
-n <- n_vec[scenario]
+n = n_vec[scenario]
 
 ###############################################################
 ## run simulation
 ###############################################################
 
 set.seed(scenario * 42)
-reject <- logical(nsim)
+reject = rep(NA, nsim)
 
-for (i in seq_len(nsim)) {
-  group1    <- rnorm(n, mean = 0,     sd = sigma)
-  group2    <- rnorm(n, mean = delta,  sd = sigma)
-  reject[i] <- t.test(group1, group2)$p.value < alpha
+for (i in 1:nsim) {
+  group1    = rnorm(n, mean = 0,     sd = sigma)
+  group2    = rnorm(n, mean = delta,  sd = sigma)
+  reject[i] = (t.test(group1, group2)$p.value < alpha)
 }
 
 ###############################################################
 ## save results
 ###############################################################
 
-results <- tibble(
+results = tibble(
   n        = n,
   delta    = delta,
   alpha    = alpha,
@@ -70,10 +71,10 @@ results <- tibble(
   scenario = scenario
 )
 
-Date <- gsub("-", "", Sys.Date())
+Date = gsub("-", "", Sys.Date())
 dir.create(file.path(here::here("results"), Date), recursive = TRUE, showWarnings = FALSE)
 
-filename <- file.path(here::here("results", Date), paste0(scenario, ".RDA"))
+filename = file.path(here::here("results", Date), paste0(scenario, ".RDA"))
 save(results, file = filename)
 
 message("Scenario ", scenario, ": n = ", n,
